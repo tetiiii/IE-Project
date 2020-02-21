9da2224032f97e89d697b4ec76fb53f62ff1d0ae
teti <fateme.k128@gmail.com>
2020-02-21 16:40:02 +0330

    add main function

---
+++ src/main/java/ir/loghme/Loghme.java
@@ -0,0 +1,13 @@
+package main.java.ir.loghme;
+import java.util.Scanner;  // Import the Scanner class
+
+public class Loghme {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);  // Create a Scanner object
+        while (scanner.hasNext()) {
+            String command = scanner.nextLine();
+            // TODO: add command parser
+
+        }
+    }
+}
