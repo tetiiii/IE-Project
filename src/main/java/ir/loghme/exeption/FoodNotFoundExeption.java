b640a63019028ae6b54d7e54ee9feeecb8348e20
teti <fateme.k128@gmail.com>
2020-03-09 18:45:00 +0330

    add custom Exeptions

---
+++ src/main/java/ir/loghme/exeption/FoodNotFoundExeption.java
@@ -0,0 +1,8 @@
+package main.java.ir.loghme.exeption;
+
+public class FoodNotFoundExeption extends Exception {
+
+    public FoodNotFoundExeption(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/ir/loghme/exeption/RestaurantNotFoundExeption.java
@@ -0,0 +1,8 @@
+package main.java.ir.loghme.exeption;
+
+public class RestaurantNotFoundExeption extends Exception {
+
+    public RestaurantNotFoundExeption(String message) {
+        super(message);
+    }
+}
