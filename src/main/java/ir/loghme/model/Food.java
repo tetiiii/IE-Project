dbeb52730a18b8b8d15144886db5b9016c34ddce
teti <fateme.k128@gmail.com>
2020-03-10 11:50:51 +0330

    add restaurantName field

--- src/main/java/ir/loghme/model/Food.java
+++ src/main/java/ir/loghme/model/Food.java
@@ -2,12 +2,14 @@ package main.java.ir.loghme.model;
 
 public class Food {
     private String name;
+    private String restaurantName;
     private String description;
     private double popularity;
     private double price;
 
     public Food() {
         this.name = "";
+        this.restaurantName = "";
         this.description = "";
         this.popularity = 0;
         this.price = 0;
@@ -24,6 +26,10 @@ public class Food {
         return name;
     }
 
+    public String getRestaurantName() {
+        return restaurantName;
+    }
+
     public String getDescription() {
         return description;
     }
@@ -38,6 +44,10 @@ public class Food {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public void setRestaurantName(String restaurantName) {
+        this.restaurantName = restaurantName;
     }
 
     public void setDescription(String description) {
