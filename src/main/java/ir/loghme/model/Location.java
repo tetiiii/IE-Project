542dcd0f5c703b608208311f8d1fa4277c7b56c8
teti <fateme.k128@gmail.com>
2020-03-17 10:25:54 +0330

    add distanceFrom method to Location

--- src/main/java/ir/loghme/model/Location.java
+++ src/main/java/ir/loghme/model/Location.java
@@ -1,5 +1,7 @@
 package main.java.ir.loghme.model;
 
+import java.lang.Math;
+
 public class Location {
     private double x;
     private double y;
@@ -17,6 +19,10 @@ public class Location {
     public Location(int x, int y) {
         this.x = x;
         this.y = y;
+    }
+
+    public double distanceFrom(Location location) {
+        return (Math.sqrt(Math.pow((this.x - location.x), 2) + Math.pow((this.y - location.y), 2)));
     }
 
     public double getX() {
