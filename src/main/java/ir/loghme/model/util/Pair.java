d01b8a4c28f5d2e71e88f4722c104d017ee4852f
teti <fateme.k128@gmail.com>
2020-03-17 10:45:54 +0330

    add generic Pair util

---
+++ src/main/java/ir/loghme/model/util/Pair.java
@@ -0,0 +1,19 @@
+package main.java.ir.loghme.model.util;
+
+public class Pair<X,Y> {
+    private final X x;
+    private final Y y;
+
+    public Pair(X x, Y y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public X getKey() {
+        return x;
+    }
+
+    public Y getValue() {
+        return y;
+    }
+}
