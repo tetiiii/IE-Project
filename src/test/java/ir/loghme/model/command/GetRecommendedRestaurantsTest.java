447ca9609ad36ab9ce10f0e53755e378bed6d6c2
teti <fateme.k128@gmail.com>
2020-03-27 00:04:52 +0430

    add success test for GetRecommendedRestaurants

--- src/test/java/ir/loghme/model/command/GetRecommendedRestaurantsTest.java
+++ src/test/java/ir/loghme/model/command/GetRecommendedRestaurantsTest.java
@@ -1,4 +1,82 @@
 package test.java.ir.loghme.model.command;
 
+import main.java.ir.loghme.controller.CommandParser;
+import main.java.ir.loghme.model.Food;
+import main.java.ir.loghme.model.Location;
+import main.java.ir.loghme.model.Restaurant;
+import main.java.ir.loghme.model.User;
+import main.java.ir.loghme.model.command.AddFood;
+import main.java.ir.loghme.model.command.GetRecommendedRestaurants;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
 public class GetRecommendedRestaurantsTest {
+    private GetRecommendedRestaurants getRecommendedRestaurants;
+
+    @BeforeEach
+    // run before every test
+    public void init() {
+        ArrayList<Restaurant> restaurants;
+        ArrayList<User> users = new ArrayList<>();
+        users.add(new User());
+
+        // add four restaurants
+        Restaurant r1 = new Restaurant("1",
+                "1",
+                new Location(3,4),
+                new ArrayList<Food>());
+
+        Restaurant r2 = new Restaurant("2",
+                "2",
+                new Location(1,2),
+                new ArrayList<Food>());
+
+        Restaurant r3 = new Restaurant("3",
+                "3",
+                new Location(1,2),
+                new ArrayList<Food>());
+
+        Restaurant r4 = new Restaurant("4",
+                "4",
+                new Location(6,9),
+                new ArrayList<Food>());
+
+        // add foods to restaurant menus
+        r1.addFood(new Food("Fateme","1","khoshmaze", 100,10000));
+        r1.addFood(new Food("Fateme2","1","khoshmaze2", 1002,100002));
+        r2.addFood(new Food("Fateme","2","khoshmaze", 100,10000));
+        r2.addFood(new Food("Fateme2","2","khoshmaze2", 1002,100002));
+        r3.addFood(new Food("Fateme","3","khoshmaze", 100,10000));
+        r3.addFood(new Food("Fateme2","3","khoshmaze2", 1,100));
+
+        restaurants = new ArrayList<Restaurant>();
+
+        // add four dummy restaurants to list of restaurants
+        restaurants.add(r1);
+        restaurants.add(r4);
+        restaurants.add(r3);
+        restaurants.add(r2);
+        this.getRecommendedRestaurants = new GetRecommendedRestaurants(restaurants, users);
+    }
+
+    @Test
+    // success test
+    public void testGetRecommendedRestaurantsSuccess() {
+        ArrayList<String> actualResult;
+        ArrayList<String> expectedResult = new ArrayList<>();
+        actualResult = this.getRecommendedRestaurants.execute(null);
+        expectedResult.add("1");
+        expectedResult.add("2");
+        expectedResult.add("3");
+
+        assertArrayEquals(expectedResult.toArray(), actualResult.toArray());
+
+    }
 }
+
