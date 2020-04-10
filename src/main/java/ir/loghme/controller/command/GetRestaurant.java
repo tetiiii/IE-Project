f0ba85bafa0599f330a0f0a5af1b59e7d955c55e
teti <fateme.k128@gmail.com>
2020-04-10 20:39:31 +0430

    change getRestaurant command to recieve Id

--- src/main/java/ir/loghme/controller/command/GetRestaurant.java
+++ src/main/java/ir/loghme/controller/command/GetRestaurant.java
@@ -3,12 +3,9 @@ package main.java.ir.loghme.controller.command;
 import main.java.ir.loghme.model.Restaurant;
 
 import java.util.ArrayList;
-import java.util.Map;
 
 public class GetRestaurant implements Command {
     private ArrayList<Restaurant> restaurants;
-    private final int ELEMENTS_NUM = 1;
-    private final String RESTAURANT_NAME_KEY = "name";
 
     public GetRestaurant(ArrayList<Restaurant> restaurants) {
         this.restaurants = restaurants;
@@ -16,32 +13,18 @@ public class GetRestaurant implements Command {
 
     @Override
     public <I, O> O execute(I input) throws ClassCastException {
-        if (!(input instanceof Map))
-            throw new ClassCastException("cannot convert input to type Map");
+        if (!(input instanceof String))
+            throw new ClassCastException("cannot convert input to type String");
 
-        Map restaurantInfo = ((Map) input);
+        String id = ((String) input);
 
-        if (restaurantInfo.size() != ELEMENTS_NUM)
-            throw new IllegalArgumentException("Invalid number of keys in JSON");
-
-        if (!(restaurantInfo.containsKey(RESTAURANT_NAME_KEY)))
-            throw new IllegalArgumentException("JSON keys are not valid ");
-
-        if (!(restaurantInfo.get(RESTAURANT_NAME_KEY) instanceof String))
-            throw new IllegalArgumentException("JSON values are not valid ");
-
-        String restaurantName = (String) restaurantInfo.get(RESTAURANT_NAME_KEY);
         Restaurant restaurant = null;
         for (Restaurant r : restaurants) {
-            if (r.getName().toLowerCase().equals(restaurantName.toLowerCase())) {
+            if (r.getId().equals(id)) {
                 restaurant = r;
                 break;
             }
         }
-
-        if (restaurant == null)
-            throw new IllegalArgumentException("No Restaurant found with this name");
-
         return (O) restaurant;
     }
 }
