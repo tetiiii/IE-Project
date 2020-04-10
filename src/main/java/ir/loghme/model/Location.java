03339991323e8e032bd4e6d8d5ff8ef438ce3bf1
teti <fateme.k128@gmail.com>
2020-04-10 20:37:31 +0430

    add toString method to Location

--- src/main/java/ir/loghme/model/Location.java
+++ src/main/java/ir/loghme/model/Location.java
@@ -25,6 +25,11 @@ public class Location {
         return (Math.sqrt(Math.pow((this.x - location.x), 2) + Math.pow((this.y - location.y), 2)));
     }
 
+    @Override
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+
     public double getX() {
         return x;
     }
