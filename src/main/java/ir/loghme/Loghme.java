ef959206386138873dbd84ceef77f3612e1ef7ce
teti <fateme.k128@gmail.com>
2020-04-10 20:35:26 +0430

    remove CommandParser and use WebServer instead in main

--- src/main/java/ir/loghme/Loghme.java
+++ src/main/java/ir/loghme/Loghme.java
@@ -1,24 +1,25 @@
 package main.java.ir.loghme;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import main.java.ir.loghme.controller.CommandParser;
+import main.java.ir.loghme.controller.WebServer;
 import main.java.ir.loghme.exeption.HttpException;
 import main.java.ir.loghme.model.Restaurant;
 import main.java.ir.loghme.model.User;
-import main.java.ir.loghme.controller.command.Command;
 import main.java.ir.loghme.model.util.adapter.HttpAdapter;
-import main.java.ir.loghme.model.util.model.Pair;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;  // Import the Scanner class
 
 public class Loghme {
     public static void main(String[] args) {
-        int RETRY_NUM = 5;
         // Initialization
+        int RETRY_NUM = 5;
         ArrayList<Restaurant> restaurants = new ArrayList<>();
+        ArrayList<User> users = new ArrayList<>();
+        users.add(new User());
+
+        // Get restaurants info from specific url (external API)
         HttpAdapter adapter = new HttpAdapter();
         for(int i = 0; i <= RETRY_NUM; i++) {
             if(i == RETRY_NUM) {
@@ -38,48 +39,16 @@ public class Loghme {
             }
         }
 
-        ArrayList<User> users = new ArrayList<>();
-        CommandParser commandParser = new CommandParser(restaurants, users);
-        users.add(new User());
-
-        Scanner scanner = new Scanner(System.in);  // Create a Scanner object
-        Pair<Command,Object> command;
-        while (scanner.hasNextLine()) {
-            String input = scanner.nextLine();
-            if (input.length() == 0) continue;
-
-            try {
-                command = commandParser.parse(input);
-
-            } catch (IllegalArgumentException e) {
-                System.err.println(e.getMessage());
-                continue;
-            }
-            try {
-                // Pair: (commandObj, JSONInputObj)
-                // Key: commandObj, Value: JSONInputObj
-                Object output = command.getKey().execute(command.getValue());
-                if(output != null)
-                      System.out.println((new ObjectMapper()).writeValueAsString(output));
-
-            } catch (IllegalArgumentException e) {
-                System.err.println(e.getMessage());
-                continue;
-
-            } catch (ClassCastException e) {
-                System.err.println("command input mismatch");
-
-            } catch (IllegalStateException e) {
-                System.err.println("program entered invalid state");
-                System.err.println(e.getMessage());
-                e.printStackTrace();
-                System.exit(1);
-
-            } catch (Exception e) {
-                System.err.println("Unknown error");
-                e.printStackTrace();
-                System.exit(1);
-            }
+        // define a Javalin Web Server and set routs and their handlers
+        WebServer wb = new WebServer(6969);
+        wb.start();
+        try {
+            // define http routs and their corresponding handlers
+            wb.configurePaths(restaurants, users);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+
     }
+
 }
